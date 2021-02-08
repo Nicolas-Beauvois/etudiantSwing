d998eba2a1bc7a2147b15fa28b577c9ef0042ec8
afpa <afpa@POR7400350.exchange.ad.afpanet>
2021-02-08 11:01:19 +0100

    Initialisation projet

---
+++ etudiantSwing/src/main/java/fr/formation/afpa/dao/EtudiantDaoFile.java
@@ -0,0 +1,24 @@
+package fr.formation.afpa.dao;
+
+import java.util.List;
+
+import fr.formation.afpa.model.Etudiant;
+
+public class EtudiantDaoFile implements IEtudiantDao{
+
+	public List<Etudiant> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void add(Etudiant e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Etudiant update(Etudiant e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ etudiantSwing/src/main/java/fr/formation/afpa/model/Etudiant.java
@@ -0,0 +1,5 @@
+package fr.formation.afpa.model;
+
+public class Etudiant {
+
+}
